4a9f81380b1a86fbbd4ff1d5605b7f652a003d39
twarbelow <taija.warbelow@gmail.com>
2021-07-07 16:02:53 -0700

    authenticate user, happy path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,12 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back, #{user.name}"
+      redirect_to dashboard_path
+    else
+
+    end
+  end
 end
