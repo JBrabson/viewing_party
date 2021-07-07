1617bcd8bbb15b2c1be355f06f150706b13ff23c
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-07 00:03:03 -0600

    Add to view, create sessions controller. WIP

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -15,19 +15,19 @@ RSpec.describe 'As a user' do
     end
 
     it 'displays username field' do
-      expect(page).to have_field('Username')
+      page.has_field?('Username', type: 'username')
     end
 
     it 'displays email field' do
-      expect(page).to have_field('Email')
+      page.has_field?('Email', type: 'email')
     end
 
     it 'displays password field' do
-      expect(page).to have_field('Password')
+      page.has_field?('Password', type: 'password')
     end
 
     it 'displays password confirmation field' do
-      expect(page).to have_field('Password')
+      page.has_field?('Password Confirmation', type: 'password_confirmation')
     end
 
     it 'displays button to register' do
@@ -37,13 +37,14 @@ RSpec.describe 'As a user' do
 
   describe 'When I complete registration successfully, it' do
     it 'redirects to user dashboard' do
-      fill_in 'user[username]', with: 'The Movie Master'
-      fill_in 'user[email]', with: 'moviemaster@email.net'
+      fill_in 'user[username]', with: 'Movie Master'
+      fill_in "user[email]", with: 'moviemaster@email.net'
       fill_in 'user[password]', with: 'password123'
       fill_in 'user[password_confirmation]', with: 'password123'
 
       click_button('Register')
       expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content('Welcome Movie Master!')
     end
   end
 
