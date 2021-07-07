54c7a664c7aae11a466c61f05ba6a02e779020ad
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-07-06 19:38:45 -0600

    Test edit for more coverage. Routes refactored using path helpers.

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome Page' do
-    it 'displays welcome message' do
-      visit '/'
-      expect(page).to have_content(@welcome_message)
-    end
+  before :each do
+    visit root_path
+  end
 
-    it 'displays application description' do
-      visit '/'
-      expect(page).to have_content(@description)
-    end
+  it 'displays welcome message' do
+    expect(page).to have_content(@welcome_message)
+  end
 
-    it 'displays login fields for username and password' do
-      visit '/'
-      page.has_field?('Username', type: 'username')
-      page.has_field?('Password', type: 'password')
-    end
+  it 'displays application description' do
+    expect(page).to have_content(@description)
+  end
 
-    it 'displays button to log in' do
-      visit '/'
-      expect(page).to have_button("Login")
-    end
+  it 'displays login fields for username and password' do
+    page.has_field?('Username', type: 'username')
+    page.has_field?('Password', type: 'password')
+  end
 
-    it 'displays link to register' do
-      visit '/'
-      expect(page).to have_link("Ready to Join the Party? Register Here")
+  it 'displays button to log in' do
+    expect(page).to have_button("Login")
+  end
 
-      click_link("Ready to Join the Party? Register Here")
-      expect(current_path).to eq(registration_path)
-    end
+  it 'displays link to register' do
+    expect(page).to have_link("Ready to Join the Party? Register Here")
+
+    click_link("Ready to Join the Party? Register Here")
+    expect(current_path).to eq(registration_path)
+  end
 end
