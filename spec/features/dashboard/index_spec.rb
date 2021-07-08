c40c30ebb4d2484d7c87dc17f34d06d232ced83e
twarbelow <taija.warbelow@gmail.com>
2021-07-07 21:02:49 -0700

    fix session issue with current_user

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe 'user dashboard' do
   before :each do
     user = User.create(email: 'test@app.com', name: 'n4me', password: 'passw0rd')
-    session[:user_id] = user.id
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     visit dashboard_path
   end
 
   it 'has personalized welcome message' do
-    expect(page).to have_content("Welcome #{user.name}!")
+    expect(page).to have_content("Welcome #{@user.name}!")
   end
 
   # it 'has expected links' do
