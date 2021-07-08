1505dc4e797fd5e45d8283b59b43b6f9f0b17b65
twarbelow <taija.warbelow@gmail.com>
2021-07-07 21:04:30 -0700

    fix variable scope

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'user dashboard' do
   before :each do
-    user = User.create(email: 'test@app.com', name: 'n4me', password: 'passw0rd')
+    @user = User.create(email: 'test@app.com', name: 'n4me', password: 'passw0rd')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     visit dashboard_path
   end
