6a2e7ff85233c0bd67d3c2e35ca0439c3a04565c
twarbelow <taija.warbelow@gmail.com>
2021-07-08 18:45:08 -0700

    spec for friends section
    Co-authored-by: Taija Warbelow <10294841+twarbelow@users.noreply.github.com>
    Co-authored-by: Jennifer Brabson <72179421+JBrabson@users.noreply.github.com>

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'user dashboard' do
   before :each do
-    @user = User.create(email: 'test@app.com', name: 'n4me', password: 'passw0rd')
+    @user = User.create!(email: 'test@app.com', name: 'n4me', password: 'passw0rd')
+    @friend = User.create!(email: 'friend@app.com', name: 'fr1end', password: 'p4ssword')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     visit dashboard_path
   end
@@ -15,8 +16,32 @@ RSpec.describe 'user dashboard' do
     expect(page).to have_link('Discover Movies')
   end
 
-  it 'has a friends section' do
-    expect(page).to have_content('Your Friends')
+  describe 'has a friends section' do
+    it 'has a header' do
+      expect(page).to have_content('Your Friends')
+    end
+
+    it 'displays message when you have no friends' do
+      within('#friends') do
+        expect(page).to have_content('You currently have no friends.')
+      end
+    end
+
+    it 'displays friends list if the user has friends' do
+      Friendship.create!(user_id: @user.id, friend_id: @friend.id)
+      within('#friends') do
+        expect(page).to have_content("#{@friend.name}")
+      end
+    end
+
+    # describe 'with a place to add friends'
+    #   it 'with a valid email' do
+    #
+    #   end
+    #
+    #   it 'but not with an invalid email' do
+    #
+    #   end
   end
 
   it 'has a viewing parties section' do
