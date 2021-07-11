3641febe37dd76c8a4689b9d049af85b96fe5ed5
twarbelow <taija.warbelow@gmail.com>
2021-07-10 20:07:14 -0700

    index action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   def index
+    if params[:movie_title].present?
+      MovieFacade.search_by_title(params[:movie_title])
+    else
+      MovieFacade.top_40
+    end
     redirect_to discover_path
   end
 end
