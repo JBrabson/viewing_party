b083e9053b7cb7772714781c3f871e55f6e0deff
twarbelow <taija.warbelow@gmail.com>
2021-07-12 06:56:07 -0700

    rubocop fixes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:movie_title].present?
-      @search_results =  MovieFacade.search_by_title(params[:movie_title])
+      @search_results = MovieFacade.search_by_title(params[:movie_title])
     else
-      @top_40 = MovieFacade.top_40
+      @top40 = MovieFacade.top_40
     end
   end
 end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,15 +1,14 @@
 class MovieService
   def self.search_movies_by_title(movie_title)
-    response_1 = conn.get("/3/search/movie?query=#{movie_title}")
-    response_1_json = JSON.parse(response_1.body, symbolize_names: true)
-    response_2 = conn.get("/3/search/movie?query=#{movie_title}&page=2")
-    response_2_json = JSON.parse(response_2.body, symbolize_names: true)
-    response_1_json[:results] + response_2_json[:results]
+    response1 = conn.get("/3/search/movie?query=#{movie_title}")
+    response1_json = JSON.parse(response1.body, symbolize_names: true)
+    response2 = conn.get("/3/search/movie?query=#{movie_title}&page=2")
+    response2_json = JSON.parse(response2.body, symbolize_names: true)
+    response1_json[:results] + response2_json[:results]
   end
 
-  private
-  def self.conn()
-    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+  def self.conn
+    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.params['api-key'] = ENV['MOVIE_API_KEY']
     end
   end
