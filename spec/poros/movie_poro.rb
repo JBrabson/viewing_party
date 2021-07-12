d6422c4e0078e2c3881158ab4acb6c81a9138178
twarbelow <taija.warbelow@gmail.com>
2021-07-11 20:19:15 -0700

    movie poro

---
+++ spec/poros/movie_poro.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe MoviePoro do
+  it 'instantiates with expected attributes' do
+    star_wars_json = File.read("#{Rails.root}/spec/fixtures/moviedb_api/star_wars_search_results_pg_1.json")
+    single_search_result = JSON.parse(star_wars_json, symbolize_names: true)[:results].first
+    movie_poro = MoviePoro.new(single_search_result)
+    expect(movie_poro.title).to be_a(String)
+    expect(movie_poro.title).to eq("Star Wars")
+    expect(movie_poro.vote_average).to be_a(float)
+    expect(movie_poro.vote_average).to eq(8.2)
+    expect(movie_poro.id).to be_a(integer)
+    expect(movie_poro.id).to eq(11)
+  end
+end
