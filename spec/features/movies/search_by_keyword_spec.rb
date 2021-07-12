5dda19dcc08781f7e25548854ce23de359d3665d
twarbelow <taija.warbelow@gmail.com>
2021-07-11 20:36:53 -0700

    update spec for 2 api pings

--- spec/features/movies/search_by_keyword_spec.rb
+++ spec/features/movies/search_by_keyword_spec.rb
@@ -6,20 +6,34 @@ feature 'search movies by keyword' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     visit discover_path
   end
+
   it 'returns results' do
     search_term = 'star wars'
-    response_body = File.open("#{Rails.root}/spec/fixtures/moviedb_api/star_wars_search_results.json")
-    stub_request(
-      :get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIE_API_KEY']}&query=#{search_term}&"
-    ).to_return(
-      status: 200, body: response_body
-    )
+    response_body_1 = File.open("#{Rails.root}/spec/fixtures/moviedb_api/star_wars_search_results_pg_1.json")
+    response_body_2 = File.open("#{Rails.root}/spec/fixtures/moviedb_api/star_wars_search_results_pg_2.json")
+
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api-key=5f797e906ade46b8521c83edea255f00&query=star%20wars")
+         .with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.4.1'
+           })
+         .to_return(status: 200, body: response_body_1, headers: {})
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api-key=5f797e906ade46b8521c83edea255f00&page=2&query=star%20wars").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.4.1'
+           }).
+         to_return(status: 200, body: response_body_2, headers: {})
     fill_in :movie_title, with: search_term
     click_button 'Search Movies'
     expect(current_path).to eq(discover_path)
     expect(page).to have_content("Results for #{search_term}:")
     within('#results') do
-      response.body[:results].each do |result|
+      response_body[:results].each do |result|
         expect(page).to have_link(result[:title])
         expect(page).to have_content("Vote Average: #{result[:vote_average]}")
       end
