8516b08d53d45bc1f05a51260beedbae1bfbd2ea
twarbelow <taija.warbelow@gmail.com>
2021-07-12 11:54:52 -0700

    make conn a private method

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe MovieService do
   it 'can search moviedb by movie title' do
     search_term = "star wars"
     response_body_1 = File.open("#{Rails.root}/spec/fixtures/moviedb_api/star_wars_search_results_pg_1.json")
+
     stub_request(:get, "https://api.themoviedb.org/3/search/movie?api-key=#{ENV['MOVIE_API_KEY']}&query=star%20wars")
          .with(
            headers: {
