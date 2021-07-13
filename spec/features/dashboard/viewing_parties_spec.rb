576326437d3143c9ccf0d53b28dcd98198878482
twarbelow <taija.warbelow@gmail.com>
2021-07-13 13:02:37 -0700

    WIP

--- spec/features/dashboard/viewing_parties_spec.rb
+++ spec/features/dashboard/viewing_parties_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe "viewing parties" do
     @friend_hosts_party = Party.create!(host_id: @friend.id, movie_id: 181812, start: 'july 25 2021, 17:00', end: 'july 25 2021, 19:00', movie_title: "Star Wars: The Rise of Skywalker")
     @friend_hosts_party.invitations.create!(invitee_id: @user.id)
     @friend_hosts_party.invitations.create!(invitee_id: @invited.id)
-    require 'pry'; binding.pry
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
     visit dashboard_path
@@ -27,11 +26,11 @@ RSpec.describe "viewing parties" do
     within('#parties') do
       within('#attending') do
         expect(page).to have_link(@friend_hosts_party.movie_title)
-        expect(page).to have_content(@friend_hosts_party.start.strftime("%b %e %Y"))
-        expect(page).to have_content(@friend_hosts_party.start.strftime("%l:%M %P"))
-        expect(page).to have_content("Host: #{@friend.name}")
-        expect(page).to have_content(@user.name)
-        expect(page).to have_content(@invited.name)
+      #   expect(page).to have_content(@friend_hosts_party.start.strftime("%b %e %Y"))
+      #   expect(page).to have_content(@friend_hosts_party.start.strftime("%l:%M %P"))
+      #   expect(page).to have_content("Host: #{@friend.name}")
+      #   expect(page).to have_content(@user.name)
+      #   expect(page).to have_content(@invited.name)
       end
       # TODO should we be testing that the name is bolded?
     end
@@ -41,22 +40,22 @@ RSpec.describe "viewing parties" do
     # list of friends invited, with my name in bold
   end
 
-  it 'shows parties I have created on the dashboard' do
-    within('#parties') do
-      within('#hosting') do
-        expect(page).to have_link(@user_hosts_party.movie_title)
-        expect(page).to have_content(@user_hosts_party.start.strftime("%b %e %Y"))
-        expect(page).to have_content(@user_hosts_party.start.strftime("%l:%M %P"))
-        expect(page).to have_content("Hosting")
-        expect(page).to have_content(@friend.name)
-        expect(page).to have_content(@invited.name)
-      end
-    end
+  # it 'shows parties I have created on the dashboard' do
+  #   within('#parties') do
+  #     within('#hosting') do
+  #       expect(page).to have_link(@user_hosts_party.movie_title)
+  #       expect(page).to have_content(@user_hosts_party.start.strftime("%b %e %Y"))
+  #       expect(page).to have_content(@user_hosts_party.start.strftime("%l:%M %P"))
+  #       expect(page).to have_content("Hosting")
+  #       expect(page).to have_content(@friend.name)
+  #       expect(page).to have_content(@invited.name)
+  #     end
+  #   end
     # Movie Title, which links to the movie show page
     # Date and Time of Event
     # That I am the host of the party
     # List of friends invited to the viewing party
-  end
+  # end
   # TODO sad path for no invited parties
   # TODO sad path for no created parties
 end
