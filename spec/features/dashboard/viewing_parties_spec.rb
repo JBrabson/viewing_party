59f5be0d386db8609253c08956dd4e43e1b859be
twarbelow <taija.warbelow@gmail.com>
2021-07-13 22:12:19 -0700

    remove comments

--- spec/features/dashboard/viewing_parties_spec.rb
+++ spec/features/dashboard/viewing_parties_spec.rb
@@ -23,9 +23,9 @@ RSpec.describe "viewing parties" do
   end
 
   it 'shows parties I am invited to on the dashboard' do
-    save_and_open_page
     within('#parties') do
       within('#attending') do
+        expect(page).to have_content("Attending")
         expect(page).to have_link(@friend_hosts_party.movie_title)
         expect(page).to have_content(@friend_hosts_party.start.strftime("%D"))
         expect(page).to have_content(@friend_hosts_party.start.strftime("%l:%M %P"))
@@ -37,22 +37,20 @@ RSpec.describe "viewing parties" do
     end
   end
 
-  # it 'shows parties I have created on the dashboard' do
-  #   within('#parties') do
-  #     within('#hosting') do
-  #       expect(page).to have_link(@user_hosts_party.movie_title)
-  #       expect(page).to have_content(@user_hosts_party.start.strftime("%b %e %Y"))
-  #       expect(page).to have_content(@user_hosts_party.start.strftime("%l:%M %P"))
-  #       expect(page).to have_content("Hosting")
-  #       expect(page).to have_content(@friend.name)
-  #       expect(page).to have_content(@invited.name)
-  #     end
-  #   end
-    # Movie Title, which links to the movie show page
-    # Date and Time of Event
-    # That I am the host of the party
-    # List of friends invited to the viewing party
-  # end
+  it 'shows parties I have created on the dashboard' do
+    within('#parties') do
+      within('#hosting') do
+        expect(page).to have_content("Hosting")
+        expect(page).to have_link(@user_hosts_party.movie_title)
+        expect(page).to have_content(@user_hosts_party.start.strftime("%D"))
+        expect(page).to have_content(@user_hosts_party.start.strftime("%l:%M %P"))
+        expect(page).to have_content(@friend.name)
+        expect(page).to have_content(@invited.name)
+      end
+    end
+  end
   # TODO sad path for no invited parties
   # TODO sad path for no created parties
+  # TODO sad path for no guests invited to your party?
+  # TODO facade for partyinfo poro? could then make all parties into poros with correct info and send to view to be displayed by date
 end
