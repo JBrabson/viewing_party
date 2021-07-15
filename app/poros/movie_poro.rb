9774cef90a5bb1197d66edf9ac8e727ed657b984
twarbelow <taija.warbelow@gmail.com>
2021-07-14 21:46:14 -0700

    refactor with initialize options hash and formatted_runtime method

--- app/poros/movie_poro.rb
+++ app/poros/movie_poro.rb
@@ -1,9 +1,23 @@
 class MoviePoro
-  attr_reader :title, :vote_average, :id
+  attr_reader :title, :vote_average, :id, :runtime, :summary, :cast, :reviews
 
-  def initialize(info_hash)
-    @title = info_hash[:title]
-    @vote_average = info_hash[:vote_average]
-    @id = info_hash[:id]
+  def initialize(options = {})
+    options[:search_hash] ||= {}
+    options[:details_hash] ||= {}
+
+    @title = options[:search_hash][:title] || options[:details_hash][:title]
+    @vote_average = options[:search_hash][:vote_average]
+    @id = options[:search_hash][:id] || options[:details_hash][:id]
+    @runtime = options[:details_hash][:runtime] || nil
+    @summary = options[:details_hash][:summary] || ""
+    @cast = options[:cast_hash] || {}
+    @reviews = options[:reviews] || {}
+  end
+
+  def formatted_runtime
+    minutes = @runtime
+    hours = minutes / 60
+    remaining_minutes = minutes % 60
+    "#{hours}:#{remaining_minutes}"
   end
 end
