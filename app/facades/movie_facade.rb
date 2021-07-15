e434088542858c271310a309ba58331dbca43b22
twarbelow <taija.warbelow@gmail.com>
2021-07-15 00:24:02 -0700

    implement cast and review poros

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -8,9 +8,19 @@ class MovieFacade
 
   def self.movie_details(movie_id)
     movie_details = MovieService.get_movie_details(movie_id)
-    cast = MovieService.get_movie_details("#{movie_id}/credits")
-    reviews = MovieService.get_movie_details("#{movie_id}/reviews")
-    MoviePoro.new(details_hash: movie_details, cast_hash: cast, reviews_hash: reviews)
+    # cast = MovieService.get_movie_details("#{movie_id}/credits")
+    # reviews = MovieService.get_movie_details("#{movie_id}/reviews")
+    # MoviePoro.new(details_hash: movie_details, cast_hash: cast, reviews_hash: reviews)
+
+    first_ten_cast = cast.take(10).map do |actor_info|
+      CastPoro.new(actor_info)
+    end
+
+    review_collection = reviews.map do |review|
+      ReviewPoro.new(review_info)
+    end
+    
+    MoviePoro.new(details_hash: movie_details, cast: first_ten_cast, reviews: review_collection)
   end
 
   def self.top40
